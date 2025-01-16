045f24a51bf6fd33bf2f034959899cc12a104181
Tomasz Granowski <155786692+granovskyyy@users.noreply.github.com>
2025-01-16 12:09:02 +0100

    zmiany zmiany

--- projektmain.m
+++ projektmain.m
@@ -1,21 +1,32 @@
-[x, fs] = audioread('czapus.m4a'); %freestyle kolegi 
+[x, fs] = audioread('azdokonca.mp3'); %piosenka kolegi do testu 
 [x1,fs1] = audioread("testaudio.m4a"); %głosowka z dyktafonu
 [x2,fs2] = audioread("20hztest.mp3"); %test dzwieku o niskiej czestotliwosci
 [x3,fs3] = audioread("20khztest.mp3"); %test dziweku o wysokiej czestotliwosci
 
 N = length(x); %długosc wektora x 
 t = (0:N-1) / fs; %wektor czasu 
+if size(x, 2) > 1 %sprawdzenie czy kanał jest mono czy stereo 
+    x = x(:,1);
+end
 
 N1= length(x1);
 t1 = (0:N1-1) / fs1;
+if size(x1, 2) > 1
+    x1 = x1(:,1);
+end
 
 N2= length(x2);
 t2 = (0:N2-1) / fs2;
-x2=x2(:,1); %bierzemy jeden kanał (dźwięk stereo)
+if size(x2, 2) > 1
+    x2 = x2(:,1);
+end
+
 
 N3= length(x3);
 t3 = (0:N3-1) / fs3;
-x3=x3(:,1);
+if size(x3, 2) > 1
+    x3 = x3(:,1);
+end
 
 S = fft(x);
 Stest = S(1:N/2);
@@ -42,20 +53,20 @@ subplot(4,1,1);
 plot(t, x, 'g'); %dziedzina czasu 
 xlabel('Czas (s)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie czasu');
+title('Analiza sygnału audio - piosenki w dziedzinie czasu');
 subplot(4,1,2);
 plot(f, s, 'b');  %dziedzina czestotliwosci 
-xlabel('Częstotliwość (kHz)');
+xlabel('Częstotliwość (10kHz)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie częstotliwości');
+title('Analiza sygnału audio - piosenki w dziedzinie częstotliwości');
 subplot(4,1,3);
 spectrogram(x, 1024, 512, 1024, fs, 'yaxis');   %spektogram 
-title("Spektrogram sygnału");
+title("Spektrogram piosenki");
 xlabel("Czas (s)");
-ylabel("Częstotliwość (kHz)");
+ylabel("Częstotliwość (10kHz)");
 subplot(4,1,4);
 histogram(x); %histogram
-title("histogram sygnału audio")
+title("Histogram piosenki")
 
 
 figure
@@ -63,57 +74,57 @@ subplot(4,1,1);
 plot(t1, x1, 'g');
 xlabel('Czas (s)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie czasu');
+title('Sygnał audio - nagranie z dyktafonu w dziedzinie czasu');
 subplot(4,1,2);
 plot(f1, s1, 'b');
 xlabel('Częstotliwość (kHz)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie częstotliwości');
+title('Sygnał audio - nagranie z dyktafonu w dziedzinie częstotliwości');
 subplot(4,1,3);
 spectrogram(x1, 1024, 512, 1024, fs1, 'yaxis');
-title("Spektrogram sygnału");
+title("Spektrogram sygnału - nagrania z dyktafonu");
 xlabel("Czas (s)");
-ylabel("Częstotliwość (kHz)");
+ylabel("Częstotliwość (10kHz)");
 subplot(4,1,4);
 histogram(x1);
-title("histogram sygnału audio")
+title("Histogram sygnału audio - nagrania z dyktafonu")
 
 figure
 subplot(4,1,1);
 plot(t2, x2, 'g');
 xlabel('Czas (s)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie czasu');
+title('Sygnał audio o częstotliwości 20Hz w dziedzinie czasu');
 subplot(4,1,2);
 plot(f2, s2, 'b');
-xlabel('Częstotliwość (kHz)');
+xlabel('Częstotliwość (10kHz)');
 ylabel('Amplituda');
-title('Sygnał audio w domenie częstotliwości');
+title('Sygnał audio o czestotliwosci 20Hz w dziedzinie częstotliwości');
 subplot(4,1,3);
 spectrogram(x2, 1024, 512, 1024, fs2, 'yaxis');
-title("Spektrogram sygnału");
+title("Spektrogram sygnału audio o częstotliwości 20Hz");
 xlabel("Czas (s)");
-ylabel("Częstotliwość (kHz)");
+ylabel("Częstotliwość (10kHz)");
 subplot(4,1,4);
 histogram(x2);
-title("histogram sygnału audio")
+title("Histogram sygnału audio o częstotliwości 20Hz")
 
 figure
 subplot(4,1,1);
 plot(t3, x3, 'g');
 xlabel('Czas (s)');
 ylabel('Amplituda');
-title('Sygnał audio - dzwięk o częstotliwosci 20kHz w domenie czasu');
+title('Sygnał audio - dzwięk o częstotliwosci 20kHz w dziedzinie czasu');
 subplot(4,1,2);
 plot(f3, s3, 'b');
-xlabel('Częstotliwość (kHz)');
+xlabel('Częstotliwość (10kHz)');
 ylabel('Amplituda');
-title('Sygnał audio - dżwięk o częstotliwości 20kHz w domenie częstotliwości');
+title('Sygnał audio - dżwięk o częstotliwości 20kHz w dziedzinie częstotliwości');
 subplot(4,1,3);
 spectrogram(x3, 1024, 512, 1024, fs3, 'yaxis');
-title("Spektrogram sygnału");
+title("Spektrogram sygnału audio o częstotliwosci 20kHz");
 xlabel("Czas (s)");
-ylabel("Częstotliwość (kHz)");
+ylabel("Częstotliwość (10kHz)");
 subplot(4,1,4);
 histogram(x3);
-title("Histogram sygnału audio")
+title("Histogram sygnału audio o częstotliwości 20kHz")
